b4577048eef55b0670d15513684e9ecbba28dfe7
Mimansha Singh <mimansha0501@gmail.com>
2026-08-03 19:10:13 +0530

    Configure Django allowed hosts for EC2

--- habot_backend/settings.py
+++ habot_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3o!lbc&ybqkcy8i&*lac6l4xne+$@0&^)(e68)p+%isj0xl1br
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.110.115.127", "localhost", "127.0.0.1"]
 
 
 # Application definition
